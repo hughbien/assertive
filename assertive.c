b4dc03189f38c2fa23b2b6c16be17b78721a528d
Hugh Bien <hugh@hughbien.com>
2012-01-15 00:33:20 -0800

    Refactor for less globals.

--- assertive.c
+++ assertive.c
@@ -4,11 +4,9 @@
 
 static void (*assert_test_fns[ASSERTIVE_MAX_TESTS])();
 static const char *assert_test_names[ASSERTIVE_MAX_TESTS];
+static int assert_test_flags[ASSERTIVE_MAX_TESTS];
 static int assert_test_count = 0;
 static int assert_test_index = 0;
-static int assert_test_failed = 0;
-static int assert_test_passes = 0;
-static int assert_test_fails = 0;
 static char assert_test_buffer[ASSERTIVE_MAX_BUFFER];
 
 static int assert_test_continue(int argc, char *argv[], const char *name) {
@@ -27,41 +25,47 @@ void assert_add_(void (*fn)(), const char *name) {
 }
 
 int assert_run(int argc, char *argv[]) {
+  int passes = 0;
+  int fails = 0;
+
   for (assert_test_index = 0; assert_test_index < assert_test_count; assert_test_index++) {
     if (assert_test_continue(argc, argv, assert_test_names[assert_test_index])) {
       continue;
     }
-    assert_test_failed = 0;
+    assert_test_flags[assert_test_index] = 0;
     assert_test_fns[assert_test_index]();
-    if (assert_test_failed) {
+    if (assert_test_flags[assert_test_index]) {
       printf("F");
-      assert_test_fails++;
+      fails++;
     } else {
       printf(".");
-      assert_test_passes++;
+      passes++;
     }
   }
 
-  if (assert_test_fails > 0) {
-    printf("\n\n%s\nPasses: %d, Fails: %d\n",
-        assert_test_buffer, assert_test_passes, assert_test_fails);
+  if (fails > 0) {
+    printf("\n\n%s\nPasses: %d, Fails: %d\n", assert_test_buffer, passes, fails);
     return 0;
   } else {
-    printf("\n\nOK (%d tests)\n", assert_test_passes);
+    printf("\n\nOK (%d tests)\n", passes);
     return 1;
   }
 }
 
 void assert_fail(const char *file, int line, const char *message) {
   char temp[ASSERTIVE_MAX_STRING];
+  int index, fails = 0;
+  for (index = 0; index < assert_test_index; index++) {
+    if (assert_test_flags[index]) { fails++; }
+  }
   snprintf(temp, ASSERTIVE_MAX_STRING, "%d) %s@%s:%d %s\n", 
-      assert_test_fails + 1,
+      fails + 1,
       assert_test_names[assert_test_index],
       file,
       line,
       message);
   strcat(assert_test_buffer, temp);
-  assert_test_failed = 1;
+  assert_test_flags[assert_test_index] = 1;
 }
 
 int assert_true_(const char *file, int line, int cond) {
